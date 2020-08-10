1a9f34684e8905cd9d28b7b9fcf810199752141b
sergeyfrolenko <sergeyfrolenko@ro.ru>
2020-08-10 14:55:26 +0300

    v0.2 add some content

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const ejs = require("gulp-ejs");
+const rename = require('gulp-rename');
 const sass = require("gulp-sass");
 // const cssnano = require("gulp-cssnano");
 const concat = require("gulp-concat");
@@ -34,8 +36,18 @@ gulp.task("pug", (done) => {
     .pipe(gulp.dest("dest"));
   done();
 });
+gulp.task("ejs", (done) => {
+  gulp
+    .src(["dev/ejs/**/*.ejs", "!dev/ejs/piece/*"])
+    .pipe(ejs())
+    //.pipe(prettier())
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest("dest"));
+  done();
+});
 gulp.watch("dev/scss/**/*.scss", gulp.series("sass"));
 gulp.watch("dev/js/*.js", gulp.series("js"));
 gulp.watch("dev/pug/*.pug", gulp.series("pug"));
+gulp.watch("dev/ejs/**/*.ejs", gulp.series("ejs"));
 
-gulp.task("default", gulp.series("sass", "js", "pug"));
+gulp.task("default", gulp.series("sass", "js", "pug", "ejs"));
