c6a12f31ff99e864cf8e95547760c665fc009c11
sergeyfrolenko <sergeyfrolenko@ro.ru>
2020-09-13 08:30:10 +0300

    change pug on ejs setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require("gulp");
-const pug = require("gulp-pug");
 const sass = require("gulp-sass");
+const ejs = require("gulp-ejs");
 // const cssnano = require("gulp-cssnano");
 const concat = require("gulp-concat");
 // const uglify = require("gulp-uglifyjs");
@@ -26,16 +26,16 @@ gulp.task("js", function (done) {
     .pipe(gulp.dest("dest/javascripts"));
   done();
 });
-gulp.task("pug", (done) => {
+gulp.task("ejs", (done) => {
   gulp
-    .src("dev/pug/*.pug")
-    .pipe(pug())
+    .src("dev/ejs/*.ejs")
+    .pipe(ejs())
     .pipe(prettier())
     .pipe(gulp.dest("dest"));
   done();
 });
 gulp.watch("dev/scss/**/*.scss", gulp.series("sass"));
 gulp.watch("dev/js/*.js", gulp.series("js"));
-gulp.watch("dev/pug/*.pug", gulp.series("pug"));
+gulp.watch("dev/ejs/*.ejs", gulp.series("ejs"));
 
-gulp.task("default", gulp.series("sass", "js", "pug"));
+gulp.task("default", gulp.series("sass", "js", "ejs"));
